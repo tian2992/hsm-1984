19743771362a7234da8d322b182f2b0d2cab3f73
Sebastian Oliva <code@sebastianoliva.com>
2017-06-07 02:46:05 -0600

    lolwut, disable paralaxer

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -9,13 +9,67 @@ class GameState extends Phaser.State {
 		this.music = this.game.add.audio('theme');
         this.music.play();
 
-		new Parallaxer(this.game, 0, 0, 2380, 480, 'background', -0.5, 0);
-		new Parallaxer(this.game, 0, this.game.world.bounds.height - 70, 2380, 70, 'foreground', -2, 0);
+		//new Parallaxer(this.game, 0, 0, 2380, 480, 'background', -0.5, 0);
+		//new Parallaxer(this.game, 0, this.game.world.bounds.height - 70, 2380, 70, 'foreground', -2, 0);
+
+		this.tileSize = 70;
+
+		//TODO: functionize
+		//initiate groups
+		this.floors = this.game.add.group();
+		this.floors.enableBody = true;
+		var newItem;
+
+		for(var i=0; i<12; i++) {
+			newItem = this.floors.create(i * this.tileSize, this.game.world.height - this.tileSize, 'foreground');
+			newItem.body.immovable = true;
+			newItem.body.velocity.x = this.levelSpeed;            
+		}
+
+		this.lastFloor = newItem;
+		
+
+		//Add the playa
+    	this.player = this.game.add.sprite(
+			this.game.world.centerX, 
+			200,
+			//this.game.world.height - (this.spacing * 2 + (3 * this.tileHeight)),
+			'playa');
+	
+		//Set the players anchor point to be in the middle horizontally
+		//this.player.anchor.setTo(0.5, 1.0);
+	
+		//Enable physics on the player
+		this.game.physics.arcade.enable(this.player);
+	
+		//Make the player fall by applying gravity
+		this.player.body.gravity.y = 0.2;
+	
+		//Make the player collide with the game boundaries 
+		this.player.body.collideWorldBounds = true;
+	
+		//Make the player bounce a little
+		this.player.body.bounce.y = 0.1;
+
+		//enable physics on the player
+		this.game.physics.arcade.enable(this.player);
+
+		//player gravity
+		this.player.body.gravity.y = 1000;
+
 
 		let text = new RainbowText(this.game, center.x, center.y, "Hot Sugar Project!");
 		text.anchor.set(0.5);
 	}
 
-}
+	update() {
+		this.game.physics.arcade.collide(this.player, this.floors);
+	
+		if(this.player.body.position.y >= this.game.world.height - this.player.body.height){
+			//todo fix
+			console.log("sucks");
+		}
+	}
+ }
 
 export default GameState;
--- src/states/LoadState.js
+++ src/states/LoadState.js
@@ -9,6 +9,8 @@ class LoadState extends Phaser.State {
         this.load.image('background', 'assets/sprites/backgrounds/background.png');
         this.load.image('foreground', 'assets/sprites/backgrounds/groundGrass.png');
 
+        this.load.spritesheet('playa', 'assets/sprites/players/Player/player_tilesheet.png', 80, 110, 20 );
+        
         this.load.audio('theme', ['assets/audio/1984.mp3']);
     };
 
