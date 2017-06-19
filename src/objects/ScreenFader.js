cb63b5d859155c9d527308e16929b2374acde118
riktothepast <rik@502studios.net>
2017-06-18 21:53:33 -0600

    feature/Fade: Adds 'section' fader. - Allows to set fading speed and fade color.

---
+++ src/objects/ScreenFader.js
@@ -0,0 +1,26 @@
+const Phaser = require('Phaser');
+
+class ScreenFader extends Phaser.Sprite {
+  constructor (game, center, asset, color, frame) {
+    super(game, center.x, center.y, asset, frame);
+    this.game = game;
+    this.alpha = 0;
+    this.tint = color;
+    this.scale.setTo(this.game.world.width, this.game.world.height);
+    this.game.stage.addChild(this);
+  }
+
+  fadeIn (speed) {
+    const tween = this.game.add.tween(this);
+    tween.to({ alpha: 1 }, speed);
+    tween.start();
+  }
+
+  fadeOut (speed) {
+    const tween = this.game.add.tween(this);
+    tween.to({ alpha: 0 }, speed);
+    tween.start();
+  }
+}
+
+export default ScreenFader;
--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,11 +1,14 @@
 import Parallaxer from 'entities/Parallaxer';
 import Player from 'entities/Player';
+import ScreenFader from 'objects/ScreenFader';
 const Phaser = require('Phaser');
 
 let player;
 let timer;
 let currentTime;
 let states;
+let fader;
+let prettyTime;
 
 class GameState extends Phaser.State {
   create () {
@@ -15,16 +18,18 @@ class GameState extends Phaser.State {
     this.music.play();
     this.background = this.game.add.group();
     this.entities = this.game.add.group();
+    this.overlays = this.game.add.group();
     this.createBackgrounds(states[0].state);
     player = new Player(this.game, center, 'playa');
     this.entities.add(player);
+    fader = new ScreenFader(this.game, {x: 0, y: 0}, 'progressBar', '#F0000');
     currentTime = 0;
     timer = this.game.time.create(false);
     timer.loop(1000, this.checkState, this);
     timer.start();
   }
 
-  createBackgrounds (state)	{
+  createBackgrounds (state) {
     this.background.forEach((bg) => {
       this.background.remove(bg);
     });
@@ -41,15 +46,27 @@ class GameState extends Phaser.State {
 
   }
 
+  secondstoMinutes (value) {
+    let minutes = Math.floor(value / 60);
+    let seconds = value % 60;
+    let stringMinutes = minutes.toString().length > 1 ? minutes : `0${minutes}`;
+    let stringSeconds = seconds.toString().length > 1 ? seconds : `0${seconds}`;
+    return `${stringMinutes}:${stringSeconds}`;
+  }
+
   checkState () {
     currentTime++;
+    prettyTime = this.secondstoMinutes(currentTime);
+    this.game.debug.text(prettyTime, 10, 20);
     states.forEach((item) => {
       let state = item.state;
-      if (state.time === currentTime) {
+      if (state.time === prettyTime) {
         console.log(`doing state change! ${state.time}   ${currentTime}`);
+        fader.fadeIn(800);
         return player.bounceOutOfScene(() => {
           this.createBackgrounds(state);
           player.swapAssets(state.player);
+          fader.fadeOut(500);
         });
       }
     });
